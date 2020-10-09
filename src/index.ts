2dcfe7078b16d555c06831543f7341a481c29212
Charles Coeurderoy <charles.coeurderoy@algoan.com>
2020-10-09 11:17:01 +0200

    chore: add a public folder

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import * as express from 'express';
+import * as path from 'path';
 
 const app: express.Application = express();
 const port: number = 3000;
 
+/**
+ * Expose the index.html file
+ */
+app.use(express.static('public'));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`App listening on port http://localhost:${port}`);
 });
