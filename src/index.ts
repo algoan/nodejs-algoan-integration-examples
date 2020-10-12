0609535b1411a6020baffcb6763b6c4264cfece4
Charles Coeurderoy <charles.coeurderoy@algoan.com>
2020-10-12 17:42:35 +0200

    feat: implement the first version of the score integration

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,45 @@
-import { BanksUser, RequestBuilder } from '@algoan/rest';
+import { Aden, BanksUser, BanksUserStatus, RequestBuilder } from '@algoan/rest';
 import * as delay from 'delay';
 import * as express from 'express';
-import * as path from 'path';
+import * as exphbs from 'express-handlebars';
+import { StatusCodes } from 'http-status-codes';
+import { config } from 'node-config-ts';
 
 const app: express.Application = express();
 const port: number = 3000;
+const defaultDelay: number = 500;
 
-const algoanRequest: RequestBuilder = new RequestBuilder('https://api.preprod.algoan.com', {
-  clientId: '',
-  clientSecret: '',
+/**
+ * Initiate a Request Builder instance
+ * This class handles OAuth2 authentication and is able to refresh tokens
+ */
+const algoanRequest: RequestBuilder = new RequestBuilder(config.algoan.baseUrl, {
+  clientId: config.algoan.clientId,
+  clientSecret: config.algoan.clientSecret,
 });
+
+let banksUserId: string;
+let aden: Aden | undefined;
 
 /**
  * Expose the index.html file
  */
-app.use(express.static('public'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
+/**
+ * GET /
+ * Renders the index.html page
+ */
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.render('index');
 });
 
+/**
+ * GET /redirect
+ * Start of the aggregation process
+ * Redirect the user if the banks user owns a "redirectUrl"
+ */
 app.get('/redirect', async (req: express.Request, res: express.Response) => {
   /**
    * Create a new Banks User
@@ -36,7 +56,11 @@ app.get('/redirect', async (req: express.Request, res: express.Response) => {
     },
   });
 
-  const banksUserId: string = response.id;
+  /**
+   * Store locally the banks-users id value
+   * Will be used to update the BanksUser status and retrieve the score
+   */
+  banksUserId = response.id;
 
   /**
    * Try to get the redirect URL
@@ -46,19 +70,69 @@ app.get('/redirect', async (req: express.Request, res: express.Response) => {
   let count: number = 0;
 
   do {
-    await delay(500);
+    await delay(defaultDelay);
     const banksUser: BanksUser = await BanksUser.getBanksUserById(banksUserId, algoanRequest);
     redirectUrl = banksUser.redirectUrl;
     count++;
   } while (redirectUrl === undefined && count < retryCount);
 
   if (redirectUrl === undefined) {
-    res.status(404).send({
+    res.status(StatusCodes.NOT_FOUND).send({
       message: 'Redirect URL not found',
     });
   }
 
   res.redirect(redirectUrl);
+});
+
+/**
+ * Callback URL called by Budget Insight when the aggregation process is finished
+ * Update the Banks User status to trigger accounts and transactions retrieval
+ */
+app.get('/callback', async (req: express.Request, res: express.Response) => {
+  const code: string = req.query.code as string;
+
+  /**
+   * Update the BanksUser status
+   */
+  const banksUser: BanksUser = await BanksUser.getBanksUserById(banksUserId, algoanRequest);
+  await banksUser.update(
+    {
+      status: BanksUserStatus.CONNECTION_COMPLETED,
+    },
+    code,
+  );
+
+  res.render('index');
+});
+
+/**
+ * GET /aden
+ * Retrieves ADEN and displays the result if it is defined
+ */
+app.get('/aden', async (req: express.Request, res: express.Response) => {
+  /**
+   * Try to get the aden property
+   */
+  const retryCount: number = 10;
+  let count: number = 0;
+
+  do {
+    await delay(defaultDelay);
+    const banksUser = await BanksUser.getBanksUserById(banksUserId, algoanRequest);
+    aden = banksUser.aden;
+    count++;
+  } while (aden === undefined && count < retryCount);
+
+  if (aden === undefined) {
+    res.status(StatusCodes.NOT_FOUND).send({
+      message: 'ADEN not found',
+    });
+  }
+
+  res.render('index', {
+    aden: JSON.stringify(aden),
+  });
 });
 
 app.listen(port, () => {
